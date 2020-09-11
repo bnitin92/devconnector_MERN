2333d83fb9a865012d450dc91d33d0540908932a
Nitin Bhandari <bnitin92@gmail.com>
2020-09-10 21:22:35 -0600

    Login and register validation

---
+++ validation/is-empty.js
@@ -0,0 +1,22 @@
+// defining a global isEmpty function to check for if any datatype is empyt
+
+/*
+// defined by function
+function isEmpty(value) {
+    return (
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length ===0)
+    );
+}
+*/
+
+// defined by arrow function
+const isEmpty = (value) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
---
+++ validation/login.js
@@ -0,0 +1,29 @@
+// all the rules for login validation
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // For anything required, make these statements to make them empty
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email invaild";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
